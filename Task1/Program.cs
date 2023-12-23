2bc5e51c934f06dc411ca3a5b26d565e1bcb6cca
Barotov Mustafo <barotov2075@gmail.com>
2023-12-23 17:59:43 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Task1. OOP 4 kism dorad 1.Abstraction 2.Encapsulation 3.Inheritance 4.Polymarphyzm.
+//Task2. Class nakshai object ast.
+//Task3. Fields in varuayble ast, Properties.
+//Task4. Be constructor mo object sohta nametonem.
+//Task5. Encapsulation baroi chizhoi hassosro yane vazhniro az digaron bajo kardan.
+//Task6. access modifiers 3 to ast 1.Private 2.Public 3.Pritected ammo mo hozirashda 2 tasha megzarem 1.Public yane kushodahay va berun az class istifoda burda metonem 2.Private yane mahkaman va berun az class istifoda burda nametonem
+
---
+++ Task2/Program.cs
@@ -0,0 +1,95 @@
+﻿
+var Post1 = new Post();
+Post1.Title = "In behtarin ast";
+Post1.Description = "Man kushish kardam";
+Post1.LikeCount = 20;
+Post1.Comments = "Mo bo shumo hastem";
+
+
+var Post2 = new Post();
+Post2.Title = "In bad ast";
+Post2.Description = "Man kushish nakardam";
+Post2.LikeCount = 230;
+Post2.Comments = "Mo bo shumo nestem";
+
+
+
+var Post3 = new Post();
+Post3.Title = "In behtarin nest";
+Post3.Description = "Man kushish karda budam";
+Post3.LikeCount = 200;
+Post3.Comments = "Mo bo shumo hastem";
+
+
+
+var Post4 = new Post();
+Post4.Title = "In behtarin ast";
+Post4.Description = "Man kushish kardam";
+Post4.LikeCount = 120;
+Post4.Comments = "Mo bo shumo hastem";
+
+
+
+var Post5 = new Post();
+Post5.Title = "In behtarin ast";
+Post5.Description = "Man kushish kardam";
+Post5.LikeCount = 720;
+Post5.Comments = "Mo bo shumo hastem";
+
+
+
+
+
+System.Console.WriteLine($"{Post1.Title}");
+System.Console.WriteLine($"{Post1.Description}");
+System.Console.WriteLine($"{Post1.LikeCount}");
+System.Console.WriteLine($"{Post1.Comments}");
+
+System.Console.WriteLine($"{Post2.Title}");
+System.Console.WriteLine($"{Post2.Description}");
+System.Console.WriteLine($"{Post2.LikeCount}");
+System.Console.WriteLine($"{Post2.Comments}");
+
+System.Console.WriteLine($"{Post3.Title}");
+System.Console.WriteLine($"{Post3.Description}");
+System.Console.WriteLine($"{Post3.LikeCount}");
+System.Console.WriteLine($"{Post3.Comments}");
+
+System.Console.WriteLine($"{Post4.Title}");
+System.Console.WriteLine($"{Post4.Description}");
+System.Console.WriteLine($"{Post4.LikeCount}");
+System.Console.WriteLine($"{Post4.Comments}");
+
+System.Console.WriteLine($"{Post5.Title}");
+System.Console.WriteLine($"{Post5.Description}");
+System.Console.WriteLine($"{Post5.LikeCount}");
+System.Console.WriteLine($"{Post5.Comments}");
+
+
+
+
+
+class Post
+{
+    public string Title;
+    public string Description;
+    public int LikeCount;
+    public string Comments;
+    public bool IsPublished ;
+
+    public string Publish()
+    {
+        return "The post is published.";
+        IsPublished = true;
+    }
+   
+    public string Like()
+    {
+        return $"{LikeCount + 1}";
+    }
+    public string Comment(string message)
+    {
+        return $"{Comments = message}";
+    }
+    
+}
